61226604e9d1c25bc82d4f184e84b00f67c9e87a
sj970329 <xox_s2@naver.com>
2021-11-10 23:14:00 +0900

    .
    edit...

--- js/script.js
+++ js/script.js
@@ -109,19 +109,9 @@ jQuery(function($)
 
 // bxSlider
 $(document).ready(function(){
-	// var slider = $('.bxslider').bxSlider({
-	// 		auto: true, mode:'fade',
-	// 	});
-	// var slider_01 =	$('.bxslider_01').bxSlider({
-	// 		auto: true,autoControls: true, mode:'vertical',
-	// 	});
 	var slider_02 =	$('.bxslider_02').bxSlider({
 			auto: true,controls:false,pagerCustom: '#bx-pager1'
 	});
-	// var slider_03 =	$('.bxslider_03').bxSlider({
-	// 		auto: true,controls:false,pager:false,maxSlides: 3,moveSlides:1,  slideWidth: 100,slideMargin:0,autoHover:true,
-	// 	});
-		// 클릭시 멈춤 현상 해결 //
 		$(document).on('click','.bx-next, .bx-prev',function() {
 		slider_02.stopAuto();
 		slider_02.startAuto();
@@ -135,25 +125,7 @@ $(document).ready(function(){
 
 // nowSlider
 $(document).ready(function(){
-	// var slider = $('.bxslider').bxSlider({
-	// 		auto: true, mode:'fade',
-	// 	});
-	// var slider_01 =	$('.bxslider_01').bxSlider({
-	// 		auto: true,autoControls: true, mode:'vertical',
-	// 	});
 	var slider_03 =	$('.bxslider_03').bxSlider({
 			auto: true, mode:'fade',
 	});
-	// var slider_03 =	$('.bxslider_03').bxSlider({
-	// 		auto: true,controls:false,pager:false,maxSlides: 3,moveSlides:1,  slideWidth: 100,slideMargin:0,autoHover:true,
-	// 	});
-		// 클릭시 멈춤 현상 해결 //
-		// $(document).on('click','.bx-next, .bx-prev',function() {
-		// slider_02.stopAuto();
-		// slider_02.startAuto();
-		// });
-		// $(document).on('mouseover','.bx-pager, #bx-pager1',function() {
-		// slider_02.stopAuto();
-		// slider_02.startAuto();
-		// });	
 });
