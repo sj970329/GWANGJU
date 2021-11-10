bedd773d910511fc4490f883eae95d2d578444ff
sj970329 <xox_s2@naver.com>
2021-11-10 23:03:48 +0900

    .
    edit

--- js/script.js
+++ js/script.js
@@ -117,7 +117,7 @@ $(document).ready(function(){
 	// 	});
 	var slider_02 =	$('.bxslider_02').bxSlider({
 			auto: true,controls:false,pagerCustom: '#bx-pager1'
-		});
+	});
 	// var slider_03 =	$('.bxslider_03').bxSlider({
 	// 		auto: true,controls:false,pager:false,maxSlides: 3,moveSlides:1,  slideWidth: 100,slideMargin:0,autoHover:true,
 	// 	});
@@ -129,80 +129,31 @@ $(document).ready(function(){
 		$(document).on('mouseover','.bx-pager, #bx-pager1',function() {
 		slider_02.stopAuto();
 		slider_02.startAuto();
-		});	
+	});	
 });
 
 
-// btSlider
-
-var imgWidth = document.querySelector(".btSlider img").width;
-		var gallMove = document.querySelector(".btSlider ul");
-		var gallZone = document.querySelector(".btSlider");
-		var arrow = document.querySelectorAll(".arrow");
-
-		window.addEventListener('load',start);
-		gallZone.addEventListener('mouseover',stop);
-		gallZone.addEventListener('mouseout',start);
-
-		var interval;
-		var aniStartPoint;
-		var aniEndPoint;
-		var count = 0;
-		var slideon = "on";
-
-		function start(){
-			interval = setInterval(go, 5000);
-			arrow[0].style.opacity = "0";
-			arrow[1].style.opacity = "0";
-		}
-
-		function go(){
-			if(slideon == "on"){
-
-				slideon = "off";
-
-				if(count == 2) count = 0;
-
-				aniStartPoint = -imgWidth * count;
-				count++;
-				aniEndPoint = -imgWidth * count;
-
-				goAni();
-			}
-		}
-
-		function left(){
-			if(slideon == "on"){
-
-				slideon = "off";
-
-				if(count == 0) count = 2;
-
-				aniStartPoint = -imgWidth * count;
-				count--;
-				aniEndPoint = -imgWidth * count;
-
-				goAni();
-			}
-		}
-
-		function goAni(){
-			var ani = gallMove.animate([
-				{
-					transform:"translateX(" + aniStartPoint + "px)"
-				},{
-					transform:"translateX(" + aniEndPoint + "px)"
-				}
-			],1000);
-
-			ani.addEventListener('finish',function(){
-				gallMove.style.transform = "translateX(" + aniEndPoint + "px)";
-				slideon = "on";
-			});
-		}
-
-		function stop(){
-			clearInterval(interval);
-			arrow[0].style.opacity = "0.3";
-			arrow[1].style.opacity = "0.3";
-		}
+// nowSlider
+$(document).ready(function(){
+	// var slider = $('.bxslider').bxSlider({
+	// 		auto: true, mode:'fade',
+	// 	});
+	// var slider_01 =	$('.bxslider_01').bxSlider({
+	// 		auto: true,autoControls: true, mode:'vertical',
+	// 	});
+	var slider_03 =	$('.bxslider_03').bxSlider({
+			auto: true, mode:'fade',
+	});
+	// var slider_03 =	$('.bxslider_03').bxSlider({
+	// 		auto: true,controls:false,pager:false,maxSlides: 3,moveSlides:1,  slideWidth: 100,slideMargin:0,autoHover:true,
+	// 	});
+		// 클릭시 멈춤 현상 해결 //
+		// $(document).on('click','.bx-next, .bx-prev',function() {
+		// slider_02.stopAuto();
+		// slider_02.startAuto();
+		// });
+		// $(document).on('mouseover','.bx-pager, #bx-pager1',function() {
+		// slider_02.stopAuto();
+		// slider_02.startAuto();
+		// });	
+});
